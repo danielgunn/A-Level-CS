9017e29de68ff4ebe62bd23305109303ec24ad5c
Daniel Gunn <dangunn@gmail.com>
2020-11-19 15:01:45 +0800

    Adding a GUI using the PySimpleGUI library

--- Chapter 20/shunting_and_evaluate.py
+++ Chapter 20/shunting_and_evaluate.py
@@ -1,3 +1,42 @@
+import PySimpleGUI as sg
+
+def make_buttons_from_line(text_line):
+    r = []
+    for c in text_line:
+        r.append(sg.Button(c))
+    return r
+
+def main():
+    layout = [
+        [sg.Text("0 " + " " * 30, key="-ANS-")],
+        make_buttons_from_line("789()"),
+        make_buttons_from_line("456/*"),
+        make_buttons_from_line("123+-"),
+        make_buttons_from_line("0=")
+    ]
+
+    # Create the window
+    window = sg.Window("Gunn Calculator", layout)
+
+    # Create an event loop
+    ans = ""
+    while True:
+        event, values = window.read()
+        # End program if user closes window
+        if event == sg.WIN_CLOSED:
+            break
+        elif event in "0123456789":
+            ans += event
+            window["-ANS-"].update(ans)
+        elif event in "+/-*()":
+            ans += " " + event + " "
+            window["-ANS-"].update(ans)
+        elif event == "=":
+            ans = str(evaluate_expression(ans))
+            window["-ANS-"].update(ans)
+
+    window.close()
+
 class Stack:
     def __init__(self):
         self.stack = []
@@ -19,64 +58,64 @@ class Stack:
     def is_empty(self):
         return len(self.stack) == 0
 
-operators_precedence = {"+":1,"-":1,"*":2,"/":2, ")":0, "(":0}
+def evaluate_expression(line_in):
+    operators_precedence = {"+":1,"-":1,"*":2,"/":2, ")":0, "(":0}
 
-line_in = input("Please input a math expression with spaces seperating all tokens:")
+    infix_tokens = line_in.split()
+    #print("PREFIX",infix_tokens)
 
-infix_tokens = line_in.split()
-#print("PREFIX",infix_tokens)
-
-# Shunting-Yard Algorithm
-operator_stack = Stack()
-postfix_tokens = []
-depth = 0  # how many layers deep of brackets are we?
-for x in infix_tokens:
-    #print("Token:",x)
-    if x in operators_precedence.keys():
-        if x == "(":
-            depth += 1
-            operator_stack.push(x)
-        elif x == ")":
-            while (not operator_stack.is_empty()):
-                op = operator_stack.pop()
-                if op == "(":
-                    depth -= 1
-                    break
-                else:
-                    postfix_tokens.append(op)
+    # Shunting-Yard Algorithm
+    operator_stack = Stack()
+    postfix_tokens = []
+    depth = 0  # how many layers deep of brackets are we?
+    for x in infix_tokens:
+        #print("Token:",x)
+        if x in operators_precedence.keys():
+            if x == "(":
+                depth += 1
+                operator_stack.push(x)
+            elif x == ")":
+                while (not operator_stack.is_empty()):
+                    op = operator_stack.pop()
+                    if op == "(":
+                        depth -= 1
+                        break
+                    else:
+                        postfix_tokens.append(op)
+            else:
+                while((not operator_stack.is_empty()) and (operators_precedence[operator_stack.peek()] > operators_precedence[x])):
+                    op = operator_stack.pop()
+                    if op not in ("(",")"):
+                        postfix_tokens.append(op)
+                operator_stack.push(x)
         else:
-            while((not operator_stack.is_empty()) and (operators_precedence[operator_stack.peek()] > operators_precedence[x])):
-                op = operator_stack.pop()
-                if op not in ("(",")"):
-                    postfix_tokens.append(op)
-            operator_stack.push(x)
-    else:
-        postfix_tokens.append(x)
-        #print("add3",x)
-while(not operator_stack.is_empty()):
-    op = operator_stack.pop()
-    if op not in ("(",")"):
-        postfix_tokens.append(op)
+            postfix_tokens.append(x)
+    while(not operator_stack.is_empty()):
+        op = operator_stack.pop()
+        if op not in ("(",")"):
+            postfix_tokens.append(op)
 
-print("POSTFIX",postfix_tokens)
+    print("POSTFIX",postfix_tokens)
 
-# Evaluate the RPN
-operator_stack=[]
-for x in postfix_tokens:
-    if x in operators_precedence.keys():
-        a = float(operator_stack.pop())
-        b = float(operator_stack.pop())
-        c = 0
-        if x == "+":
-            c = a + b
-        elif x == "-":
-            c = b - a
-        elif x == "*":
-            c = a * b
+    # Evaluate the RPN
+    operator_stack=[]
+    for x in postfix_tokens:
+        if x in operators_precedence.keys():
+            a = float(operator_stack.pop())
+            b = float(operator_stack.pop())
+            c = 0
+            if x == "+":
+                c = a + b
+            elif x == "-":
+                c = b - a
+            elif x == "*":
+                c = a * b
+            else:
+                c = b / a
+            operator_stack.append(c)
         else:
-            c = b / a
-        operator_stack.append(c)
-    else:
-        operator_stack.append(x)
-answer = operator_stack.pop()
-print(answer)
+            operator_stack.append(x)
+    answer = operator_stack.pop()
+    return answer
+
+main()
